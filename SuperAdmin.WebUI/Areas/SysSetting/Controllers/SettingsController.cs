d86a9300fc42d9c3df29798f40056827f6e02746
zhangchenghang <chenghang1937@126.com>
2016-01-05 17:08:00 +0800

    添加系统用户管理页面

---
+++ SuperAdmin.WebUI/Areas/SysSetting/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAdmin.WebUI.Areas.SysSetting.Controllers
+{
+    public class SettingsController : Controller
+    {
+        //系统基础设置
+        // GET: /SysSetting/Settings/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuperAdmin.WebUI/Areas/SysSetting/SysSettingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SuperAdmin.WebUI.Areas.SysSetting
+{
+    public class SysSettingAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SysSetting";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SysSetting_default",
+                "SysSetting/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
