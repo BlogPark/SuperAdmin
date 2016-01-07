e7c65698f8a3a99586b8d0a0667a9a62eb895d4b
zhangchenghang <chenghang1937@126.com>
2016-01-07 22:15:51 +0800

    添加广告

---
+++ SuperAdmin.WebUI/Areas/SysAdvertisement/Controllers/SysAdManageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAdmin.WebUI.Areas.SysAdvertisement.Controllers
+{
+    public class SysAdManageController : Controller
+    {
+        //
+        // GET: /SysAdvertisement/SysAdManage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 系统广告
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SysAdInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuperAdmin.WebUI/Areas/SysAdvertisement/SysAdvertisementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SuperAdmin.WebUI.Areas.SysAdvertisement
+{
+    public class SysAdvertisementAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SysAdvertisement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SysAdvertisement_default",
+                "SysAdvertisement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
