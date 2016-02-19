fb06f1fa80f393f4671595a4e79214a7090690bb
zhangchenghang <chenghang1937@126.com>
2016-02-19 09:37:19 +0800

    调试站内信页面

---
+++ SuperAdmin.WebUI/Areas/AdminArea/Controllers/SiteMsgController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAdmin.WebUI.Areas.AdminArea.Controllers
+{
+    public class SiteMsgController : Controller
+    {
+        //
+        // GET: /AdminArea/SiteMsg/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
