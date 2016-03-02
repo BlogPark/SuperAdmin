dcea3cd86eaf7666cb80013b50016cdc8500630f
zhangchenghang <chenghang1937@126.com>
2016-03-02 21:57:48 +0800

    ss

--- SuperAdmin.WebUI/Areas/AdminArea/Controllers/WebImgageController.cs
+++ SuperAdmin.WebUI/Areas/AdminArea/Controllers/WebImgageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SuperAdmin.DataBLL;
 using SuperAdmin.WebUI.Areas.AdminArea.Models;
 
 namespace SuperAdmin.WebUI.Areas.AdminArea.Controllers
@@ -11,10 +12,11 @@ namespace SuperAdmin.WebUI.Areas.AdminArea.Controllers
     {
         //
         // GET: /AdminArea/WebImgage/
-
+        WebSiteImageBll bll = new WebSiteImageBll();
         public ActionResult Index()
         {
             WebImgageIndexViewModel model = new WebImgageIndexViewModel();
+            model.list = bll.GetAllModel();
             return View(model);
         }
 
